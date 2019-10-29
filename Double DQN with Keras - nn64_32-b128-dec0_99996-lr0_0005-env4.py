f20d24c2abf141960f26c193298e611365066fca
crilazar <49878304+crilazar@users.noreply.github.com>
2019-10-29 10:56:14 -0400

    c

--- Double DQN with Keras - nn64_32-b128-dec0_99996-lr0_0005-env4.py
+++ Double DQN with Keras - nn64_32-b128-dec0_99996-lr0_0005-env4.py
@@ -196,7 +196,7 @@ if __name__ == '__main__':
     env = ForexTrading()
     write_to_log('---------------------------------------------')
     write_to_log('Environment loaded successfuly')
-    ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=4, epsilon_dec=0.99996, epsilon=1.0, batch_size=128, input_dims=41)
+    ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=3, epsilon_dec=0.99996, epsilon=1.0, batch_size=128, input_dims=41)
     n_games = 1000    
     ddqn_scores = []
     eps_history = []
